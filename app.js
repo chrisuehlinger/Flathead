0206adc89fcbf12ae6ca211f2d9ce6b50d5a2358
Chris Uehlinger <chris.uehlinger@gmail.com>
2015-11-11 16:32:00 -0500

    Remove console log

--- app.js
+++ app.js
@@ -23,8 +23,6 @@ var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
-console.log('heyo', process.env.NODE_ENV);
-
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
 
 // Enable CORS

b7ccd08727bd4dad6a54952e7a24282bfcbdbd6c
Chris Uehlinger <chris.uehlinger@gmail.com>
2015-11-11 15:56:33 -0500

    Prepare for publishing

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ var env = process.env.NODE_ENV || 'development';
 app.locals.ENV = env;
 app.locals.ENV_DEVELOPMENT = env == 'development';
 
+console.log('heyo', process.env.NODE_ENV);
+
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
 
 // Enable CORS

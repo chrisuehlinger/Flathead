a6b8fcdc52ade84c6927b30f6b79985d64882614
Chris Uehlinger <chris.uehlinger@gmail.com>
2015-12-03 13:25:38 -0500

    Add version number to admin interface

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var package = require('../package.json');
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.render('admin', {title: 'Flathead'});
+  res.render('admin', {title: 'Flathead', version: 'v' + package.version});
 });
 
 module.exports = router;
